b7b0e3c87339172e4d6d38af5be50e1bfa9b80f5
adithya_narisipilli <narisipilliadithya1010@gmail.com>
2024-06-30 00:10:00 +0530

    create and add the footer component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,6 +7,7 @@ import Dashboard from "./pages/Dashboard";
 import Problems from "./pages/Problems";
 import SignUp from "./pages/SignUp";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 export default function App() {
   return (
@@ -21,6 +22,7 @@ export default function App() {
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/problems" element={<Problems />} />
       </Routes>
+      <Footer />
     </BrowserRouter>
   );
 }
--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,5 +1,40 @@
 export default function About() {
   return (
-    <div>About</div>
-  )
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="max-w-2xl mx-auto p-3 text-center">
+        <div>
+          <h1 className="text-3xl font font-semibold text-center my-7">
+            About Codecalorie
+          </h1>
+          <div className="text-md text-gray-500 flex flex-col gap-6">
+            <p>
+              Welcome to Codecalorie! This website was created by Adithya
+              Narisipilli as a personal project to foster a community of
+              competitive programmers and coding enthusiasts. Adithya is a
+              passionate developer who loves solving challenging problems and
+              sharing his knowledge with others.
+            </p>
+
+            <p>
+              On this platform, you'll find a variety of coding challenges,
+              contests, and tutorials designed to help you improve your
+              problem-solving skills. Whether you're a beginner looking to learn
+              the basics or an experienced coder seeking to hone your skills,
+              there's something here for everyone.
+            </p>
+
+            <p>
+              We encourage you to participate in our contests, solve problems,
+              and engage with other members of the community. You can discuss
+              strategies, share insights, and learn from each other. We believe
+              that a collaborative environment is key to growth and improvement.
+            </p>
+            <p>
+              Happy coding!
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
