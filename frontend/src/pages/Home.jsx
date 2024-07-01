98d8e5bcbf55b215b9a9ff817c5a4f92aa3c8a82
adithya_narisipilli <narisipilliadithya1010@gmail.com>
2024-07-01 09:10:39 +0530

    complete the home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,65 @@
+import { Link } from "react-router-dom";
+import CallToAction from "../components/CallToAction";
+import { useEffect, useState } from "react";
+
 export default function Home() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const res = await fetch("/backend/post/getPosts");
+      const data = await res.json();
+      setPosts(data.posts);
+    };
+    fetchPosts();
+  }, []);
+
   return (
-    <div>Home</div>
-  )
+    <div>
+      <div className="flex flex-col gap-6 p-28 px-3 max-w-6xl mx-auto ">
+        <h1 className="text-3xl font-bold lg:text-6xl">
+          Welcome to my OnlineJudge
+        </h1>
+        <p className="text-gray-500 text-xs sm:text-sm">
+          Challenge your coding skills with our online judge platform. Compete,
+          learn, and grow with a community of coders.
+        </p>
+        <Link
+          to="/search"
+          className="text-xs sm:text-sm text-teal-500 font-bold hover:underline"
+        >
+          View all posts
+        </Link>
+      </div>
+      <div className="p-3 bg-amber-100 dark:bg-slate-700">
+        <CallToAction />
+      </div>
+
+      <div className="max-w-6xl mx-auto p-3 flex flex-col gap-8 py-7">
+        {posts && posts.length > 0 && (
+          <div className="flex flex-col gap-6">
+            <h2 className="text-2xl font-semibold text-center">Recent Posts</h2>
+            <div className="flex flex-wrap gap-4">
+              {posts.map((post) => (
+                <div key={post._id} className="w-full md:w-1/2 lg:w-1/3 p-4">
+                  <div className="p-4 bg-white dark:bg-gray-800 rounded-lg shadow-md">
+                    <h3 className="text-xl font-bold">{post.title}</h3>
+                    <p className="text-gray-600 dark:text-gray-400">
+                      {post.description}
+                    </p>
+                  </div>
+                </div>
+              ))}
+            </div>
+            <Link
+              to={"/search"}
+              className="text-lg text-teal-500 hover:underline text-center"
+            >
+              View all posts
+            </Link>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 }
