af5545b2c23aeb2022319a6720f0f8bac783edc8
adithya_narisipilli <narisipilliadithya1010@gmail.com>
2024-07-06 00:46:52 +0530

    done backend for online compiler

--- Backend/index.js
+++ Backend/index.js
@@ -7,6 +7,9 @@ import postRoutes from './routes/post.route.js';
 import problemRoutes from './routes/problem.route.js';
 import commentRoutes from './routes/comment.route.js';
 import cookieParser from 'cookie-parser';
+import { generateFile } from './generateFile.cjs';
+import { executeCpp } from './executeCpp.cjs';
+import cors from 'cors';
 
 dotenv.config();
 
@@ -21,8 +24,32 @@ mongoose
 
 const app = express();
 
-app.use(express.json());
 app.use(cookieParser());
+app.use(cors());
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+
+
+app.get("/",(req,res)=>{
+  res.json({online:"compiler"});
+});
+
+app.post("/run", async (req, res) => {
+    // const language = req.body.language;
+    // const code = req.body.code;
+
+    const { language = 'cpp', code } = req.body;
+    if (code === undefined) {
+        return res.status(404).json({ success: false, error: "Empty code!" });
+    }
+    try {
+        const filePath = await generateFile(language, code);
+        const output = await executeCpp(filePath);
+        res.json({ filePath, output });
+    } catch (error) {
+        res.status(500).json({ error: error });
+    }
+});
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000!');
